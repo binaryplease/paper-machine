26d6c67c064fe77b25cce21db5c8df8aedde230f
Pablo <pablo1@mailbox.org>
2017-11-04 17:10:31 +0100

    added midiparser structure

---
+++ midiparser.rb
@@ -0,0 +1,14 @@
+# Parse midi file and generate a to dim array with tracks
+
+class MidiParser
+								attr_accessor :mid_array
+
+								def initialize()
+																# Create midi parser object
+																@mid_array
+								end
+
+								def parse(mid_file)
+																# parse midi file to array
+								end
+end
