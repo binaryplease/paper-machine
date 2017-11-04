13c2b67ced77e0b6fdac785441ac01405a1f7efc
Pablo <pablo1@mailbox.org>
2017-11-04 17:06:15 +0100

    added chartgenerator structire

---
+++ chartgenerator.rb
@@ -0,0 +1,11 @@
+# Draw charts from two-dimenstional array
+
+class ChartGenerator
+								def initialize(page_width, circle_size, horiz_step, vert_step)
+																# Create a generator object with specified parameters
+								end
+
+								def generate(tracks)
+																# Parse 2-dim array to image
+								end
+end
